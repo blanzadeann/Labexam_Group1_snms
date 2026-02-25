f0ec9ce4c1d1538fc9bf0fae25aea8267477ff40
Ezekiel <ezekielmatias09@gmail.com>
2026-02-25 22:22:29 +0800

    Register/validation

--- app/Models/article.php
+++ app/Models/article.php
@@ -3,21 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
-    protected $fillable = [
-        'category_id',
-        'title',
-        'body',
-        'author_email',
-        'image_url',
-        'status'
-    ];
-
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(Category::class);
-    }
+    //
 }
