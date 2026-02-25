5770833bc5ce1fe4919cd1d9b188d0f27c681bf2
blanzadeannsamuel-crypto <blanzadeannsamuel@gmail.com>
2026-02-25 19:12:09 +0800

    fixed and updated authcontroller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-use Illuminate\Http\Request;
+
 
 
 class AuthController extends Controller
