5f7c83d1fc2146af29f8a55d1a0e5425f80f4cba
Eduard <hakarimalopit@gmail.com>
2026-02-25 21:50:39 +0800

    eduardtzycommit

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
-    // Developer 7: Publish article feature endpoint
-    // We use PATCH because we are only updating the 'status' field
     Route::patch('/articles/{article}/publish', [ArticleController::class, 'publish']);
 });
 
-// Developer 6: Article CRUD API
 Route::apiResource('articles', ArticleController::class);
